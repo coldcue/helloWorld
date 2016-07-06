f6a0d7041e5b49a36446ec3e72ccd813eeddf38a
Andras Szell <coldcue@gmail.com>
2016-07-06 15:22:38 +0200

    haxelib added

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var config = require('./config.json');
 
 //TODO create haxe gulp plugin
 var exec = require('child_process').exec;
+var fs = require('fs');
+var path = require('path');
 
 // Plugins
 var gutil = require('gulp-util');
@@ -14,7 +16,48 @@ var gutil = require('gulp-util');
   Build haxe sources
 */
 gulp.task('haxe', function(cb) {
-    var cmd = 'haxe build-src.hxml -js ' + config.path.dist.js + '/main.js';
+
+    // Get haxelib dependencies
+    function getHaxelibSrcList() {
+        var haxelibSrcList = [];
+
+        // Get the path of the haxelib repository
+        var dir = config.path.haxelib;
+        fs.readdirSync(dir)
+            .forEach(function(file) {
+                // This is the directory of the library
+                var libDir = path.join(dir, file);
+
+                // Check if its a directory
+                if (fs.statSync(libDir).isDirectory()) {
+                    // This is the path of the .current file in the library
+                    var currentFile = path.join(libDir, '.current')
+                    if (fs.existsSync(currentFile)) {
+                        // Get the version string
+                        var version = fs.readFileSync(currentFile, 'utf8');
+                        var srcPath = path.join(libDir, version.split('.').join(','), 'src');
+
+                        // If the srcPath exists, then add it to the array
+                        if (fs.existsSync(srcPath)) {
+                            haxelibSrcList.push(srcPath);
+                        }
+                    }
+                }
+            });
+
+        return haxelibSrcList;
+    }
+
+    var haxelibSrcList = getHaxelibSrcList();
+
+    // Add every library to the command line
+    var libSrc = '';
+
+    haxelibSrcList.forEach(function(folder) {
+        libSrc += ' -cp ' + folder;
+    });
+
+    var cmd = 'haxe ' + libSrc + ' build-src.hxml -js ' + config.path.dist.js + '/main.js';
 
     gutil.log('Executing', gutil.colors.red(cmd));
 
